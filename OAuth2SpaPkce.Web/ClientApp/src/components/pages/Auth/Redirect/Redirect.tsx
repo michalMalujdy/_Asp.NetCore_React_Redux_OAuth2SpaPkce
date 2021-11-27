c00af27f5dbf14a818a93df96c805bca295784d7
Michal Malujdy <mima@scalepoint.com>
2021-11-27 14:55:38 +0100

    Refactor Redirect

--- OAuth2SpaPkce.Web/ClientApp/src/components/pages/Auth/Redirect/Redirect.tsx
+++ OAuth2SpaPkce.Web/ClientApp/src/components/pages/Auth/Redirect/Redirect.tsx
@@ -1,38 +1,27 @@
-﻿import React, {Component} from 'react';
-import {withRouter} from 'react-router';
-import axios from 'axios';
-import qs from 'qs';
-import Card from "../../../common/Card/Card";
-import Loader from "../../../common/Loader/Loader";
+﻿import React from 'react';
+import { withRouter } from 'react-router';
+import Card from '../../../common/Card/Card';
+import Loader from '../../../common/Loader/Loader';
+import authService from '../../../../services/AuthService';
 
-class Redirect extends Component<RedirectProps, RedirectState> {
+class Redirect extends React.Component<RedirectProps, RedirectState> {
 
     constructor(props: RedirectProps) {
         super(props);
-
         this.state = { } as RedirectState;
     }
 
     async componentDidMount() {
-        const query = new URLSearchParams(window.location.search);
-        const queryUrlState = query.get('state') as string;
-        const queryCode = query.get('code') as string;
-
-        const urlState = localStorage.getItem('state') as string;
-        const isCsrfTokenValid = queryUrlState === urlState;
+        authService.readCodeFromRedirectUri(window.location.search);
 
         this.setState({
             ...this.state,
-            verifier: localStorage.getItem('verifier') as string,
-            challenge: localStorage.getItem('challenge') as string,
-            urlState: localStorage.getItem('state') as string,
-            code: queryCode,
-            isCsrfTokenValid: isCsrfTokenValid
+            verifier: authService.getVerifier(),
+            challenge: authService.getChallenge(),
+            urlState: authService.getState(),
+            code: authService.getCode(),
+            isCsrfTokenValid: authService.isCsrfTokenValid()
         });
-
-        if (!isCsrfTokenValid) {
-            return;
-        }
     }
 
     render() {
@@ -54,35 +43,13 @@ class Redirect extends Component<RedirectProps, RedirectState> {
         </React.Fragment>
     );
 
-    private async getTokens(codeVerifier: string, urlCode: string) {
-        const data = {
-            grant_type: 'authorization_code',
-            client_id: 'gxrNnJJckTJmeKYTK6GplVtEOJ0Drd9O',
-            code_verifier: codeVerifier,
-            code: urlCode,
-            redirect_uri: 'https://localhost:5001/auth/redirect'
-        };
-
-        const config: any = {
-            method: 'POST',
-            url: 'https://dev-ykfj37bm.us.auth0.com/oauth/token',
-            headers: {'content-type': 'application/x-www-form-urlencoded'},
-            data: qs.stringify(data)
-        };
-
-        return await axios.request(config);
-    }
-
     private onContinueClick = async () => {
         this.setState({
             ...this.state,
             isLoading: true
         });
 
-        const response = await this.getTokens(this.state.verifier, this.state.code);
-
-        localStorage.setItem('accessToken', response.data.access_token);
-        localStorage.setItem('idToken', response.data.id_token);
+        await authService.requestTokens(this.state.code);
 
         this.props.history.push('/auth/tokens')
     }
--- OAuth2SpaPkce.Web/ClientApp/src/services/AuthService.ts
+++ OAuth2SpaPkce.Web/ClientApp/src/services/AuthService.ts
@@ -1,14 +1,58 @@
-﻿export class AuthService {
+﻿import qs from "qs";
+import axios from "axios";
+
+export class AuthService {
     private verifierStorageKey = 'verifier';
     private challengeStorageKey = 'challenge';
     private stateStorageKey = 'state';
+    private codeStorageKey = 'code';
+    private redirectUrlStateStorageKey = 'redirectUrlState';
+    private accessTokenStorageKey = 'accessToken';
+    private idTokenStorageKey = 'idToken';
 
-    public generateVerifier = () => this.getStrongRandomValue();
-    public generateState = () => this.getStrongRandomValue();
+    public generateVerifier = (): string => {
+        return this.getStrongRandomValue();
+    };
 
-    public generateChallenge = async (verifier: string) => {
+    public getVerifier = (): string => {
+        return localStorage.getItem(this.verifierStorageKey) as string;
+    };
+
+    public generateChallenge = async (verifier: string): Promise<string> => {
         const challenge = await this.sha256(verifier);
         return this.base64UrlEncode(challenge);
+    };
+
+    public getChallenge = (): string => {
+        return localStorage.getItem(this.challengeStorageKey) as string;
+    };
+
+    public generateState = (): string => {
+        return this.getStrongRandomValue();
+    };
+
+    public getState = (): string => {
+        return localStorage.getItem(this.stateStorageKey) as string;
+    };
+
+    public getCode = (): string => {
+        return localStorage.getItem(this.codeStorageKey) as string;
+    };
+
+    public getRedirectUrlState = (): string => {
+        return localStorage.getItem(this.redirectUrlStateStorageKey) as string;
+    };
+
+    public isCsrfTokenValid = (): boolean => {
+        return !!this.getState() && this.getState() === this.getRedirectUrlState();
+    }
+
+    public getAccessToken = (): string => {
+        return localStorage.getItem(this.accessTokenStorageKey) as string;
+    };
+
+    public getIdToken = (): string => {
+        return localStorage.getItem(this.idTokenStorageKey) as string;
     };
 
     public storeCodeFlowData = (verifier: string, challenge: string, state: string) => {
@@ -32,6 +76,38 @@
             '&audience=https://dev-ykfj37bm.us.auth0.com/api/v2/' +
             `&state=${localStorage.getItem(this.stateStorageKey)}`;
     };
+
+    public readCodeFromRedirectUri = (redirectUri: string): void => {
+        const query = new URLSearchParams(redirectUri);
+
+        const code = query.get('code') as string;
+        localStorage.setItem(this.codeStorageKey, code);
+
+        const queryUrlState = query.get('state') as string;
+        localStorage.setItem(this.redirectUrlStateStorageKey, queryUrlState);
+    };
+
+    public async requestTokens(urlCode: string) {
+        const data = {
+            grant_type: 'authorization_code',
+            client_id: 'gxrNnJJckTJmeKYTK6GplVtEOJ0Drd9O',
+            code_verifier: this.getVerifier(),
+            code: urlCode,
+            redirect_uri: 'https://localhost:5001/auth/redirect'
+        };
+
+        const config: any = {
+            method: 'POST',
+            url: 'https://dev-ykfj37bm.us.auth0.com/oauth/token',
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            data: qs.stringify(data)
+        };
+
+        const response = await axios.request(config);
+
+        localStorage.setItem(this.accessTokenStorageKey, response.data.access_token);
+        localStorage.setItem(this.idTokenStorageKey, response.data.id_token);
+    }
 
     private getStrongRandomValue = (): string => {
         const array = new Uint32Array(32);
